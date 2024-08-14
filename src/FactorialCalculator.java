b6b913ef54330ab0891d8bb5f39015d16dc8dc7b
Dawood Magsi <dawoodmagsi0@gmail.com>
2024-08-14 15:54:34 +0500

    Initial

---
+++ src/FactorialCalculator.java
@@ -0,0 +1,10 @@
+package factorial;
+
+public class FactorialCalculator {
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * calculateFactorial(n - 1);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+import factorial.FactorialCalculator;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        long factorial = FactorialCalculator.calculateFactorial(number);
+        System.out.println("The factorial of " + number + " is " + factorial);
+
+        scanner.close();
+    }
+}
